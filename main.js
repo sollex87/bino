1396cec9b925789568630d21c7dd190d9190b898
Косточка Роман Андрійович <sollex@np.ua>
2018-10-23 13:58:29 +0300

    Minor changes

---
+++ main.js
@@ -0,0 +1,10 @@
+let worksButtons = document.getElementsByClassName('worksNavigation__item');
+let workRadios = document.getElementsByClassName('worksNavigation__radio');
+
+// for (i in worksButtons) {
+//     element = worksButtons[i];
+//     console.log(element);
+//         element.addEventListener('click', () => {
+//             workRadios[i].checked = true;
+//         })
+// }
